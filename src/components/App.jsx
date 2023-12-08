e29eb0858e61a30f0fab78a220aea407b3563934
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-12-07 21:59:31 -0800

    Restructure files in /src

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import "./../styles/index.css"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="text-6xl text-center">
+        CV Application
+      </p>
+    </>
+  )
+}
+
+export default App
